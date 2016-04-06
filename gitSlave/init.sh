e9d5e65ecac57f0ea9d0db5decac5e7d86a2f31d
blobb <andre@blobb.me>
2016-04-06 18:05:47 +0200

    removed secs, changed init.sh

--- gitSlave/init.sh
+++ gitSlave/init.sh
@@ -5,20 +5,20 @@
 SLAVE_ARGS=""
 # -noReconnect gives the possibility to kill Docker slaves within Jenkins by disconnecting the slave instance.
 
-if [ "$1" == "swarm" ]; then
-  INVOCATION="docker run -i -t -v `pwd`/sshhome/gitslave/.ssh gitslave02 java -jar swarmSlave.jar -noRetryAfterConnected -master http://172.17.42.1:8190 -executors 1 -fsroot /home/gitslave/ -labels git -mode exclusive -name git"
+MASTER=$1
+if [ "$2" == "swarm" ]; then
+  INVOCATION="docker run -i -t -v `pwd`/ssh/:/home/gitslave/.ssh gitslave02 java -jar swarmSlave.jar -noRetryAfterConnected -master http://172.17.42.1:8190 -executors 1 -fsroot /home/gitslave/ -labels git -mode exclusive -name git"
 else
   INVOCATION="java -jar slave.jar -noReconnect -jnlpUrl http://172.17.42.1:8190/computer/androidbuild/slave-agent.jnlp"
 fi
 
-echo $INVOCATION
+echo -e "\n$INVOCATION\n"
 
 docker run --rm \
     \
-    -v ${ANDROID_HOME}:/home/androidbuild/android-sdk \
+    -v `pwd`/ssh/:/home/gitslave/.ssh \
     \
     gitslave02 $INVOCATION
-    # androidbuild
 
 #swarm variante
 #java -jar swarmSlave.jar -noRetryAfterConnected -master http://172.17.41:8190 -executors 1 -fsroot $HOME -labels androidbuild -mode exclusive -name androidbuild
